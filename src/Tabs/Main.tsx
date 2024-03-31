18faa1bad655e00fad231e0209c11b701c82cb4f
tuddiesgames <tuddiesgames@gmail.com>
2024-03-31 20:22:59 +0530

    dhurgesh

---
+++ src/Tabs/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Home from './Home.tsx'
+import About from "./About.tsx"
+
+const Main = ()=>{
+    return(
+        <div>
+            <Home />
+
+            <About />
+        </div>
+    )
+}
+
+export default Main
